74105b38031f4f79fe5b565fff7a1c45f5a6f509
hardtme <hardtme@iastate.edu>
2023-05-02 12:24:04 -0500

    Change patience message and remove unnecessary comments

--- inst/shiny-examples/scoreLRapp/app.R
+++ inst/shiny-examples/scoreLRapp/app.R
@@ -10,9 +10,7 @@ shoedata_split <- dep_split(shoedata, 0.8, 585)
 ui <- fluidPage(
   # Application title
   titlePanel("ScoreLR App"),
-  # selectInput("select", label = h3("Select Method"), 
-  #             choices = list("Ignore Dependence" = 1, "Average Features" = 2, "Independent Set" = 3, "Multiple KDE"=4), 
-  #             selected = 1),
+  mainPanel("Please be patient. The plotting function takes a moment to run after you select a method."),
   selectizeInput(
     "select", "Select Method", choices = list("Ignore Dependence" = 1, "Average Features" = 2, 
                                               "Independent Set" = 3, "Multiple KDE" = 4),
@@ -27,37 +25,14 @@ ui <- fluidPage(
   
   # Show a plot of the SLR
   mainPanel(
-    #textOutput("patience")
   ),
   plotOutput("slrplot")
 )
 
 server <- function(input, output) {
   
-  #skipPlot <- reactiveVal(value = 1)
-  
-  # output$patience <- renderText({
-  #   if(input$select == 1){
-  #     result <- "Please be patient! Your ROC curve for the SLR fit via the Ignore Dependence method is coming soon!"
-  #   }
-  #   else if(input$select == 2){
-  #     result <- "Please be patient! Your ROC curve for the SLR fit via the Average Features method is coming soon!"
-  #   }
-  #   else if(input$select == 3){
-  #     result <- "Please be patient! Your ROC curve for the SLR fit via the Independent Set method is coming soon!"
-  #   }
-  #   else if(input$select == 4){
-  #     result <- "Please be patient! Your ROC curve for the SLR fit via the Multiple KDE method is coming soon!"
-  #   }
-  # })
-  
   output$slrplot <- renderPlot({
-    # if (isolate(skipPlot()==1)) {
-    #   # skip first reactive sequence
-    #   skipPlot(0)
-    #   # launch next reactive sequence
-    #   invalidateLater(10, session = getDefaultReactiveDomain())
-    # } else {
+
     if(input$select==1) {
       p <- plot_slr_roc(shoedata_split, "IgnoreDependence", num_runs = 10)
     }
@@ -71,7 +46,6 @@ server <- function(input, output) {
       p <- plot_slr_roc(shoedata_split, "MultipleKDE", num_runs = 10)
     }
     p
-    #}
   })
 }
 
