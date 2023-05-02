8189cef852b4b465f8735f0a6da61c4ce6e6b0e9
hardtme <hardtme@iastate.edu>
2023-05-02 12:32:55 -0500

    Let user choose the number of runs

--- inst/shiny-examples/scoreLRapp/app.R
+++ inst/shiny-examples/scoreLRapp/app.R
@@ -10,9 +10,12 @@ shoedata_split <- dep_split(shoedata, 0.8, 585)
 ui <- fluidPage(
   # Application title
   titlePanel("ScoreLR App"),
-  mainPanel("Please be patient. The plotting function takes a moment to run after you select a method."),
+  mainPanel("Please be patient. The plotting function takes a moment to run after you select the number of runs
+            and a method."),
+  numericInput("num_runs", label = h3("Number of Runs"), value = 1, min = 1, max = 200),
+  
   selectizeInput(
-    "select", "Select Method", choices = list("Ignore Dependence" = 1, "Average Features" = 2, 
+    "select", label = "Select Method", choices = list("Ignore Dependence" = 1, "Average Features" = 2, 
                                               "Independent Set" = 3, "Multiple KDE" = 4),
     options = list(
       placeholder = 'Please select a method below',
@@ -34,16 +37,16 @@ server <- function(input, output) {
   output$slrplot <- renderPlot({
 
     if(input$select==1) {
-      p <- plot_slr_roc(shoedata_split, "IgnoreDependence", num_runs = 10)
+      p <- plot_slr_roc(shoedata_split, "IgnoreDependence", num_runs = input$num_runs)
     }
     if(input$select==2) {
-      p <- plot_slr_roc(shoedata_split, "AverageFeatures", num_runs = 10)
+      p <- plot_slr_roc(shoedata_split, "AverageFeatures", num_runs = input$num_runs)
     }
     if(input$select==3) {
-      p <- plot_slr_roc(shoedata_split, "StrictIndependentSet", num_runs = 10)
+      p <- plot_slr_roc(shoedata_split, "StrictIndependentSet", num_runs = input$num_runs)
     }
     if(input$select==4) {
-      p <- plot_slr_roc(shoedata_split, "MultipleKDE", num_runs = 10)
+      p <- plot_slr_roc(shoedata_split, "MultipleKDE", num_runs = input$num_runs)
     }
     p
   })
